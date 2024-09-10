c038c9e0a89da2f6916b49558d678346d436e74b
GANESH-A08 <ganeshgani0046@gmail.com>
2024-09-10 06:42:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : [" |  Web Developer","|Frontend developer"," | UI/UX Designer","|Data Anlyst"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+
+
+
+
+
+
+
+
